378e5e2a3086814cbdc8a78200ca257a9e4cc7de
Lucas Silva dos Santos <lucas.santos9322@gmail.com>
2018-03-16 01:14:21 -0300

    adcionando o array adapter

--- agenda/app/src/main/java/br/com/workout/agenda/ListaAlunosActivity.java
+++ agenda/app/src/main/java/br/com/workout/agenda/ListaAlunosActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -15,6 +17,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //recupera o comportamento do oncreate que tem no app compat activity
         setContentView(R.layout.activity_lista_alunos); //R é um atalho para a pasta res, do projeto
+
+        //Criando os dados da lista de alunos
+        String[] alunos = {"Daniel","Ronald","Cafu","Vinicius Jr"};
+
+        //Procuro o objeto do xml pelo id
+        ListView listaAlunos = (ListView) findViewById(R.id.Lista_Alunos);
+
+        //Adcionando os alunos na listview
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+        //Escolhendo o adptador para a view
+        listaAlunos.setAdapter(adapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
