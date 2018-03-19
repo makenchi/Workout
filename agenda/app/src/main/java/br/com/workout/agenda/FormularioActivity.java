9c636f25c6f47c0c1df9b3c1e0ba3dd01dd3aea7
Lucas Silva dos Santos <lucas.santos9322@gmail.com>
2018-03-19 01:26:26 -0300

    Criando o botão de confirmar no menu

--- agenda/app/src/main/java/br/com/workout/agenda/FormularioActivity.java
+++ agenda/app/src/main/java/br/com/workout/agenda/FormularioActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -39,4 +41,10 @@ public class FormularioActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
